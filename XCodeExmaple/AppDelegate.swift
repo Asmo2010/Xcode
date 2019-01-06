25e18cbf14b5d9053b678081eee7a27a1b66b772
Sergii Bukovelo <sergiibukovelo@sergiis-imac.local>
2019-01-06 16:58:39 -0600

    Initial Commit

---
+++ XCodeExmaple/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XCodeExmaple
+//
+//  Created by Sergii Bukovelo on 1/6/19.
+//  Copyright © 2019 Sergii Bukovelo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XCodeExmaple/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCodeExmaple
+//
+//  Created by Sergii Bukovelo on 1/6/19.
+//  Copyright © 2019 Sergii Bukovelo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
